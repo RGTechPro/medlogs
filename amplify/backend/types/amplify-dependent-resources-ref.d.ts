2af314b22ee5ed6942ae16696249b41747ad1c28
Rishabh Gupta <73791635+RGTechPro@users.noreply.github.com>
2023-07-30 04:03:43 +0530

    bug fixes

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "medlogs": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "medlogs": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:flutter/material.dart';
 import 'package:medlogs/home.dart';
 import 'package:medlogs/login_page.dart';
@@ -5,8 +6,10 @@ import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:amplify_datastore/amplify_datastore.dart';
 import 'models/ModelProvider.dart';
 import 'amplifyconfiguration.dart';
+
 void main() {
-  runApp( MyApp());
+WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
 }
 
 class MyApp extends StatefulWidget {
@@ -15,38 +18,46 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-    bool _amplifyConfigured = false;
-@override
+  bool _amplifyConfigured = false;
+
+  @override
   void initState() {
- 
-        super.initState();
-    _configureAmplify();
+    super.initState();
+    initialize();
   }
-  //const MyApp({super.key});
+
+  void initialize()  {
+     _configureAmplify();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-     
-        primarySwatch: Colors.blue,
-      ),
-      home: LoginPage()
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: _amplifyConfigured ? const Home() : const LoginPage());
   }
 
-    void _configureAmplify() async {
+  void _configureAmplify() async {
+    try {      
+      
+      await Amplify.addPlugin(AmplifyAuthCognito());
 
-
-    await Amplify.addPlugin(AmplifyDataStore(modelProvider: ModelProvider.instance));
-
-    await Amplify.configure(amplifyconfig);
-    try {
+      await Amplify.addPlugin(
+          AmplifyDataStore(modelProvider: ModelProvider.instance));
+      await Amplify.configure(amplifyconfig);
       setState(() {
         _amplifyConfigured = true;
       });
     } catch (e) {
       print(e);
     }
+  }
+
+  Future<bool> isUserSignedIn() async {
+    final result = await Amplify.Auth.fetchAuthSession();
+    return result.isSignedIn;
   }
 }
